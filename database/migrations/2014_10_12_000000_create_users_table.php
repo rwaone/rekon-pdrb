bac18231ebeacf0f930ad5b08ce54b949875b014
rwaone <rwaone@gmail.com>
2023-02-09 13:12:59 +0800

    Update 2014_10_12_000000_create_users_table.php
    Editing user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('satker_id');
+            $table->foreignId('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
