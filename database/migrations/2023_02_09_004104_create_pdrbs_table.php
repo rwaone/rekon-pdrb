ff41cce06018d91d0853b16d69e54708e527be49
rwaone <rwaone@gmail.com>
2023-02-09 13:13:08 +0800

    Update 2023_02_09_004104_create_pdrbs_table.php

--- database/migrations/2023_02_09_004104_create_pdrbs_table.php
+++ database/migrations/2023_02_09_004104_create_pdrbs_table.php
@@ -15,9 +15,7 @@ return new class extends Migration
     {
         Schema::create('pdrbs', function (Blueprint $table) {
             $table->id();
-            $table->year();
-            $table->tinyInteger('quarter');
-            $table->tinyInteger('round');
+            $table->foreignId('period_id');
             $table->foreignId('subsector_id');
             $table->foreignId('region_id');
             $table->timestamps();
