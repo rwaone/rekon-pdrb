7794e194af4a8e4b86751a6ac17ce8bd80369035
rwaone <rwaone@gmail.com>
2023-02-09 14:40:55 +0800

    Migration
    editing pdrb table and categories seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,6 +14,79 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            [
+                'code' => 'A',
+                'name' => 'Pertanian, Kehutanan, dan Perikanan'
+            ],
+            [
+                'code' => 'B',
+                'name' => 'Pertambangan dan Penggalian'
+            ],
+            [
+                'code' => 'C',
+                'name' => 'Industri Pengolahan'
+            ],
+            [
+                'code' => 'D',
+                'name' => 'Pengadaan Listrik dan Gas'
+            ],
+            [
+                'code' => 'E',
+                'name' => 'Pengadaan Air, Pengelolaan Sampah, Limbah, dan Daur Ulang'
+            ],
+            [
+                'code' => 'F',
+                'name' => 'Konstruksi'
+            ],
+            [
+                'code' => 'G',
+                'name' => 'Perdagangan Besar dan Eceran; Reparasi Mobil dan Sepeda Motor'
+            ],
+            [
+                'code' => 'H',
+                'name' => 'Transportasi dan Pergudangan'
+            ],
+            [
+                'code' => 'I',
+                'name' => 'Penyediaan Akomodasi dan Makan Minum'
+            ],
+            [
+                'code' => 'J',
+                'name' => 'Informasi dan Komunikasi'
+            ],
+            [
+                'code' => 'K',
+                'name' => 'Jasa Keuangan dan Asuransi'
+            ],
+            [
+                'code' => 'L',
+                'name' => 'Real Estate'
+            ],
+            [
+                'code' => 'M,N',
+                'name' => 'Jasa Perusahaan'
+            ],
+            [
+                'code' => 'O',
+                'name' => 'Administrasi Pemerintahan, Pertahanan dan Jaminan Sosial Wajib'
+            ],
+            [
+                'code' => 'P',
+                'name' => 'Jasa Pendidikan'
+            ],
+            [
+                'code' => 'Q',
+                'name' => 'Jasa Kesehatan dan Kegiatan Sosial'
+            ],
+            [
+                'code' => 'R,S,T,U',
+                'name' => 'Jasa Lainnya'
+            ],
+        ];
+
+        foreach($categories as $category){
+            Category::create($category)
+        }
     }
 }
