1fe1bc5bd998f2b1169ff9661fa1d7d0b4a131e8
rwaone <35188258+rwaone@users.noreply.github.com>
2023-03-23 00:30:08 +0800

    Update RegionSeeder.php

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -19,82 +19,82 @@ class RegionSeeder extends Seeder
             [
                 'satker_id' => '7100',
                 'code' => '7100',
-                'name' => 'BPS Provinsi Sulawesi Utara'
+                'name' => 'Provinsi Sulawesi Utara'
             ],
             [
                 'satker_id' => '7101',
                 'code' => '7101',
-                'name' => 'BPS Kab. Bolaang Mongondow'
+                'name' => 'Kab. Bolaang Mongondow'
             ],
             [
                 'satker_id' => '7102',
                 'code' => '7102',
-                'name' => 'BPS Kab. Minahasa'
+                'name' => 'Kab. Minahasa'
             ],
             [
                 'satker_id' => '7103',
                 'code' => '7103',
-                'name' => 'BPS Kab. Kepulauan Sangihe'
+                'name' => 'Kab. Kepulauan Sangihe'
             ],
             [
                 'satker_id' => '7104',
                 'code' => '7104',
-                'name' => 'BPS Kab. Kepulauan Talaud'
+                'name' => 'Kab. Kepulauan Talaud'
             ],
             [
                 'satker_id' => '7105',
                 'code' => '7105',
-                'name' => 'BPS Kab. Minahasa Selatan'
+                'name' => 'Kab. Minahasa Selatan'
             ],
             [
                 'satker_id' => '7106',
                 'code' => '7106',
-                'name' => 'BPS Kab. Minahasa Utara'
+                'name' => 'Kab. Minahasa Utara'
             ],
             [
                 'satker_id' => '7107',
                 'code' => '7107',
-                'name' => 'BPS Kab. Bolaang Mongondow Utara'
+                'name' => 'Kab. Bolaang Mongondow Utara'
             ],
             [
                 'satker_id' => '7108',
                 'code' => '7108',
-                'name' => 'BPS Kab. Kepulauan Siau Tagulandang Biaro'
+                'name' => 'Kab. Kepulauan Siau Tagulandang Biaro'
             ],
             [
                 'satker_id' => '7105',
                 'code' => '7109',
-                'name' => 'BPS Kab. Minahasa Tenggara'
+                'name' => 'Kab. Minahasa Tenggara'
             ],
             [
                 'satker_id' => '7101',
                 'code' => '7110',
-                'name' => 'BPS Kab. Bolaang Mongondow Selatan'
+                'name' => 'Kab. Bolaang Mongondow Selatan'
             ],
             [
                 'satker_id' => '7174',
                 'code' => '7111',
-                'name' => 'BPS Kab. Bolaang Mongondow Timur'
+                'name' => 'Kab. Bolaang Mongondow Timur'
             ],
             [
                 'satker_id' => '7171',
                 'code' => '7171',
-                'name' => 'BPS Kota Manado'
+                'name' => 'Kota Manado'
             ],
             [
                 'satker_id' => '7172',
                 'code' => '7172',
-                'name' => 'BPS Kota Bitung'
+                'name' => 'Kota Bitung'
             ],
             [
                 'satker_id' => '7173',
                 'code' => '7173',
-                'name' => 'BPS Kota Tomohon'
+                'name' => 'Kota Tomohon'
             ],
             [
                 'satker_id' => '7174',
                 'code' => '7174',
-                'name' => 'BPS Kota Kotamobagu'
+                'name' => 'Kota Kotamobagu'
             ],
         ];
 
