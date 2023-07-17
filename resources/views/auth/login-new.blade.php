fd6f85c1aabb9aaacc8b3d00de15a2a92e60424e
rwaone <rwaone@gmail.com>
2023-07-17 10:59:23 +0800

    Update login-new.blade.php

--- resources/views/auth/login-new.blade.php
+++ resources/views/auth/login-new.blade.php
@@ -19,6 +19,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ url('') }}/dist/img/karlota-logo.png">
 </head>
 
 <body>
