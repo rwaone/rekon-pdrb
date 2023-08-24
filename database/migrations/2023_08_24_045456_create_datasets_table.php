693b4d23b094691aa9c0564dcfdf723337ac93e1
rwaone <rwaone@gmail.com>
2023-08-24 16:00:56 +0800

    new model

---
+++ database/migrations/2023_08_24_045456_create_datasets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('datasets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('period_id');
+            $table->char('status', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('datasets');
+    }
+};
