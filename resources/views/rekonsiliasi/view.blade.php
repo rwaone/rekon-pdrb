e5abadcfdd4d4b12c37b6182c7abdb26e0c1588b
rwaone <35188258+rwaone@users.noreply.github.com>
2024-02-12 20:22:38 +0800

    submit unsubmit

--- resources/views/rekonsiliasi/view.blade.php
+++ resources/views/rekonsiliasi/view.blade.php
@@ -74,7 +74,7 @@
     
     <div class="card save-container d-none">
         <div class="ml-auto">
-            <button id="submitButton" type="button" class="btn btn-success float-right" style='margin-right:5px'>Submit</button>
+            <button id="submitModalButton" type="button" class="btn btn-success float-right" style='margin-right:5px'>Submit</button>
             <button id="unsubmitButton" type="button" class="btn btn-danger float-right" style='margin-right:5px'>Batal Submit</button>
             <button id="fullFormSave" type="button" class="btn btn-warning float-right" style='margin-right:10px'>Simpan</button>
         </div>
@@ -136,6 +136,28 @@
                 </div>
             </form>
         </div>
+    </div>
+
+    <div class="modal fade" id="submit-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Konfirmasi</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah Anda Benar-Benar Ingin Mengirim Data ini?</p>
+                </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+                        <button id="submitButton" class="btn btn-success">Submit</button>
+                    </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
     </div>
 
     <x-slot name="script">
